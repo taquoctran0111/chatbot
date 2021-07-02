9b42bb77b43b7f41bca643ea9690325a313761a0
Ta Quoc Tran <taquoctrantb2000@gmail.com>
2021-07-02 15:22:35 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -213,7 +213,7 @@ let setPersistentMenu = async (req, res) => {
   // Send the HTTP request to the Messenger Platform
   await request(
     {
-      uri: `https://graph.facebook.com/v11.0/me/custom_user_settings?access_token=${PAGE_ACCESS_TOKEN}`,
+      uri: `https://graph.facebook.com/v11.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`,
       qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
       method: "POST",
       json: request_body,
