0009e8c2471096b08c9e955fb9db897df69a6f5b
Ta Quoc Tran <taquoctrantb2000@gmail.com>
2021-07-02 14:11:36 +0700

    update

--- src/services/chatbotServices.js
+++ src/services/chatbotServices.js
@@ -27,10 +27,49 @@ function callSendAPI(sender_psid, response) {
     }
   );
 }
+let getUsername = (sender_psid, response) => {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+        method: "GET",
+      },
+      (err, res, body) => {
+        if (!err) {
+          body = JSON.parse(body);
+          let username = `${body.last_name} ${body.first_name}`;
+          resolve(username);
+        } else {
+          console.error("Unable to send message:" + err);
+          reject(err);
+        }
+      }
+    );
+  });
+  // Send the HTTP request to the Messenger Platform
+  //   let username = "";
+  //   request(
+  //     {
+  //       uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+  //       qs: { access_token: PAGE_ACCESS_TOKEN },
+  //       method: "GET",
+  //     },
+  //     (err, res, body) => {
+  //       if (!err) {
+  //         response = JSON.parse(res);
+  //         username = `${response.first_name} ${response.last_name}`;
+  //       } else {
+  //         console.error("Unable to send message:" + err);
+  //       }
+  //     }
+  //   );
+  //   return username;
+};
 let handleGetStarted = (sender_psid) => {
   return new Promise(async (resolve, reject) => {
     try {
-      let response = { text: "Hello guys" };
+      let username = await getUsername(sender_psid);
+      let response = { text: `Xin chào ${username}!` };
       await callSendAPI(sender_psid, response);
       resolve("done");
     } catch (e) {
