28027056a2bec0989d16f8cfe798d8ca19228d2e
Ta Quoc Tran <taquoctrantb2000@gmail.com>
2021-07-02 17:25:31 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -130,7 +130,6 @@ async function handlePostback(sender_psid, received_postback) {
       await chatbotServices.handleGetStarted(sender_psid);
       break;
     case "COVID19":
-      response = { text: "ok" };
       await chatbotServices.handleGetDataNcov(sender_psid);
       break;
     default:
