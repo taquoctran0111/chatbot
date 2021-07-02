f32cb54ec52c084479eb401e82c7a51a0bd176e0
Ta Quoc Tran <taquoctrantb2000@gmail.com>
2021-07-02 10:40:59 +0700

    update

--- src/server.js
+++ src/server.js
@@ -11,9 +11,9 @@ viewEngine(app);
 webRoutes(app);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log("App is running at the port: " + port);
-})
+  console.log("App is running at the port: " + port);
+});
